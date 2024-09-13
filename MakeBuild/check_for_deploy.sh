6898284338372bc3c2c8e78733245d8f828ee9e4
Brox <station437@5023.ru>
2024-09-13 17:53:28 +0300

    Update check_for_deploy.sh

--- MakeBuild/check_for_deploy.sh
+++ MakeBuild/check_for_deploy.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-#set in crontab: */5 * * * *   user    cd check_deploy_path && ./check_for_deploy.sh
+### set in crontab: */5 * * * *   user    cd check_deploy_path && ./check_for_deploy.sh
 
 if [ -e .deploy ]
 then
-#add any part of process name
-pkill -f $(pwd).*any_part_of_process_name
+### add any part of process name
+#pkill -f $(pwd).*any_part_of_process_name
+#pkill -f game_binary_name
+
+### change directory
+#cd ~/gamefolder
 tar xz -f linuxserver_latest.tar.gz
-mv linuxserver_latest.tar.gz linuxserver_prev.tar.gz
 chmod a+x LinuxServer/Server.sh
 rm -f .deploy
 
-##sudo reboot
-#or
-##LinuxServer/./Server.sh &
+### after deploy
+#sudo reboot
+### or
+#LinuxServer/./Server.sh &
 
 fi
